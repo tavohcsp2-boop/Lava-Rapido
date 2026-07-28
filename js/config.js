f751b09311fd4b1b9f752574aa2af8bb4d29f53f
tavohcsp2-boop <tavohcsp2@gmail.com>
2026-07-28 18:48:01 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
    ========================================================================== */
 
 const SUPABASE_URL = 'https://huslahiuefqquzoggefh.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1c2xhaGl1ZWZxcXV6b2dnZWZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjE5Mzg3MjksImV4cCI6MjAzNzUxNDcyOX0.4wD-dM-M-0N2Yj1yM2X8J9K0L1M2N3O4P5Q6R7S8T9U';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh1c2xhaGl1ZWZxcXV6b2dnZWZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxNzYxOTcsImV4cCI6MjEwMDc1MjE5N30.igdcz9qF0VLHUPKj_j9RtQ7jI4GZrHISbDU7SkQ6yqs';
 
 // Inicialização do cliente Supabase
 let supabaseClient = null;
